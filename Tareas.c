b3b6e8d9e02c59780eeb3f5d07751a5bef5f1e88
“DuranLautaro” <lautarotuna11@gmail.com>
2026-05-03 16:57:04 -0300

    Implementacion de Funcion BuscarTareaPorI o Clave y liberacion de memoria

--- Tareas.c
+++ Tareas.c
@@ -16,10 +16,15 @@ Nodo * CrearTarea(int valor);
 void InsertarNodo(Nodo ** Tarea , Nodo *Nodo);
 Nodo * QuitarTarea(Nodo ** Tarea, int idbuscado);
 void MostrarTareas(Nodo *Tarea);
+void ConsultarTareas(Nodo *TareasPendientes,Nodo *TareasRealizadas);
+Nodo * BuscarPorId(Nodo *Tarea,int Id);
+Nodo * BuscarPorPalabraClave(Nodo *Tarea,char *palabra);
+void MostrarUnaTarea(Nodo *nodo);
+void LiberarMemoria(Nodo *Tarea);
 int main () {
     srand(time(NULL));
     Nodo * Tareaspendientes=CrearListaVacia(); //apunta a NULL
-    Nodo * TareasRealizadas=CrearListaVacia();
+    Nodo * TareasRealizadas=CrearListaVacia(); //apunta a NULL
     int id=1000;
     int seleccionar;
     /*----SOLICITUD DE TAREAS PENDIENTES*----*/
@@ -53,8 +58,13 @@ int main () {
                 scanf("%d",&idbuscado);
                 getchar();
                 Nodo * NodoAmover=QuitarTarea(&Tareaspendientes,idbuscado);
-                InsertarNodo(&TareasRealizadas,NodoAmover);
-
+                if(NodoAmover!=NULL) {
+                    /*SE INSERTA EL NODO SI Y SOLO SI EXISTE LA TAREA CON LA ID BUSCADA*/
+                    InsertarNodo(&TareasRealizadas,NodoAmover);
+                }
+                else {
+                    printf("No se encontro una tarea con el id buscado\n");
+                }
             break;
             }
         }
@@ -67,6 +77,16 @@ int main () {
     MostrarTareas(Tareaspendientes);
     printf("---Lista de Tareas Realizadas---");
     MostrarTareas(TareasRealizadas);
+
+
+    /*CONSULTA DE TAREAS POR ID Y PALABRA CLAVE*/
+    ConsultarTareas(Tareaspendientes,TareasRealizadas);
+    if(Tareaspendientes!=NULL) {
+        LiberarMemoria(Tareaspendientes);
+    }
+    if(TareasRealizadas!=NULL) {
+        LiberarMemoria(TareasRealizadas);
+    }
     return 0;
 }
 Nodo * CrearListaVacia(){
@@ -118,12 +138,130 @@ Nodo * QuitarTarea(Nodo ** Tarea, int idbuscado)
 void MostrarTareas(Nodo *Tarea) {
     Nodo *aux=Tarea;
     if(aux==NULL) {
-        printf("La lista esta vacia\n");
+        printf("\nLa lista esta vacia\n");
     }
+    else {
+        while(aux!=NULL) {
+            printf("\nTarea Id:%d",aux->T.TareaID);
+            printf("\nDescripcion:%s",aux->T.Descripcion);
+            printf("\nDuracion:%d",aux->T.Duracion);
+            aux=aux->Siguiente;
+            printf("\n");
+        }
+    }
+    
+}
+Nodo * BuscarPorId(Nodo *Tarea,int Id) {
+    Nodo *aux=Tarea;
     while(aux!=NULL) {
-        printf("\nTarea Id:%d",aux->T.TareaID);
-        printf("\nDescripcion:%s",aux->T.Descripcion);
-        printf("\nDuracion:%d",aux->T.Duracion);
+        if(aux->T.TareaID==Id) {
+            return aux;
+        }
         aux=aux->Siguiente;
+    }
+    return NULL;
+}
+Nodo * BuscarPorPalabraClave(Nodo *Tarea,char *palabra){
+    Nodo *aux=Tarea;
+    //RECORDAR QUE strstr devuelve un puntero si encuentra la palabra dentro de la frase, caso contrario devuelve NULL
+    while(aux!=NULL) {
+        if(strstr(aux->T.Descripcion, palabra)!=NULL) {
+            return aux;
+        }
+        aux=aux->Siguiente;
+    }
+    return NULL;
+}
+void MostrarUnaTarea(Nodo *nodo) {
+    if (nodo != NULL) {
+        printf("\n--- Tarea Encontrada ---");
+        printf("\nID: %d", nodo->T.TareaID);
+        printf("\nDescripcion: %s", nodo->T.Descripcion);
+        printf("\nDuracion: %d\n", nodo->T.Duracion);
+    }
+}
+void ConsultarTareas(Nodo *TareasPendientes,Nodo *TareasRealizadas) {
+    int opcion;
+    printf("Seleccionar el tipo de consulta(1:ID o 2:Palabra Clave):");
+    scanf("%d",&opcion);
+    getchar();
+    switch(opcion) {
+        case 1:
+            /*BUSQUEDA POR ID*/
+            int IDbuscada;
+            printf("Ingresar la id buscada:");
+            scanf("%d",&IDbuscada);
+            getchar();
+            /*PRIMERO BUSCO EN TAREAS PENDIENTES*/
+            if(TareasPendientes!=NULL) {
+                printf("Busco en Tareas Pendientes...\n");
+                Nodo *Buscarid=BuscarPorId(TareasPendientes,IDbuscada);
+                if(Buscarid!=NULL) {
+                    printf("Mostrar NODO\n");
+                    MostrarUnaTarea(Buscarid);
+                }
+                else {
+                    printf("No hay coincidencia\n");
+                }
+            }
+            /*BUSCO EN TAREAS REALIZADAS*/
+            if(TareasRealizadas!=NULL) {
+                printf("Busco en Tareas Realizadas...\n");
+                Nodo *Buscarid=BuscarPorId(TareasRealizadas,IDbuscada);
+                if(Buscarid!=NULL) {
+                    printf("Mostrar NODO\n");
+                    MostrarUnaTarea(Buscarid);
+                }
+                else{
+                    printf("No hay coincidencia\n");
+                }
+            }
+            if(TareasPendientes==NULL && TareasRealizadas==NULL) {
+                printf("Las listas estan vacias");
+            }
+        break;
+        case 2:
+            /*BUSQUEDA POR PALABRA CLAVE*/
+            char palabraclave[50];
+            printf("Ingresar la palabra clave:");
+            gets(palabraclave);
+            /*PRIMERO BUSCO EN TAREAS PENDIENTES*/
+            if(TareasPendientes!=NULL) {
+                printf("Busco en Tareas Pendientes...\n");
+                Nodo *BuscarPalabra=BuscarPorPalabraClave(TareasPendientes,palabraclave);
+                if(BuscarPalabra!=NULL) {
+                    printf("Mostrar NODO\n");
+                    MostrarUnaTarea(BuscarPalabra);
+                }
+                else {
+                    printf("No hay coincidencia\n");
+                }
+            }
+            /*BUSCO EN TAREAS REALIZADAS*/
+            if(TareasRealizadas!=NULL) {
+                printf("Busco en Tareas Realizadas...\n");
+                Nodo *BuscarPalabra=BuscarPorPalabraClave(TareasRealizadas,palabraclave);
+                if(BuscarPalabra!=NULL) {
+                    printf("Mostrar NODO\n");
+                    MostrarUnaTarea(BuscarPalabra);
+                }
+                else {
+                    printf("No hay coincidencia\n");
+                }
+            }
+            if(TareasPendientes==NULL && TareasRealizadas==NULL) {
+                printf("Las listas estan vacias");
+            }
+        break;
+    }
+}
+void LiberarMemoria(Nodo *Tarea) {
+    /*Genero un nodo auxiliar para poder recorrer toda la lista sin tener fuga de memoria*/
+    Nodo *aux;
+    while(Tarea!=NULL) {
+        aux=Tarea; //guardo el nodo actual
+        Tarea=Tarea->Siguiente;
+        free(aux->T.Descripcion);
+        free(aux);
     }
 }
